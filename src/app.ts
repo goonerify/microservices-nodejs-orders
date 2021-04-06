42eb38ddea24f2e02738e464c2f54dbd78eaea13
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-06 01:57:04 -0400

    329. Scaffolding a Few Route Handlers

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { createTicketRouter } from "./routes/new";
 
 import { errorHandler, NotFoundError, currentUser } from "@oldledger/common";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes/index";
-import { updateTicketRouter } from "./routes/update";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes/index";
+import { deleteOrderRouter } from "./routes/delete";
+import { createOrderRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true); // Make express aware that traffic is being proxied to this service by ingress nginx/istio, and configure express to trust that proxy
@@ -21,10 +21,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(createOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
