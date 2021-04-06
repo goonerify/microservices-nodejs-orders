99138692c555fcb01b14cc007009837c69685bdd
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-06 01:30:51 -0400

    326. Scaffolding the Orders Service

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { createTicketRouter } from "./routes/new";
+
+import { errorHandler, NotFoundError, currentUser } from "@oldledger/common";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+app.set("trust proxy", true); // Make express aware that traffic is being proxied to this service by ingress nginx/istio, and configure express to trust that proxy
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // secure: process.env.NODE_ENV !== 'test' // NOTE: Disable security
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
