5fa5682535ba8a718024a30653e10f64f1174b2f
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-06 02:05:29 -0400

    330. Subtle Service Coupling

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,9 +1,26 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@oldledger/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      // Validate id is mongo id. This introduces tight coupling with the tickets service
+      // by making assumptions about the db used to generate the ticketId
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as createOrderRouter };
