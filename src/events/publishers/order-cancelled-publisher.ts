4e89ebfcc3509d776229ecea3a96f9e5d0aac9b2
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-07 19:43:21 -0400

    355. Publishing Order Cancellation

--- src/events/publishers/order-cancelled-publisher.ts
+++ src/events/publishers/order-cancelled-publisher.ts
@@ -1,4 +1,4 @@
-import { Subjects, Publisher, OrderCancelledEvent } from '@sgtickets/common';
+import { Subjects, Publisher, OrderCancelledEvent } from "@oldledger/common";
 
 export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
   subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
