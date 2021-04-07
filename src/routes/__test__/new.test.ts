a99fa21c4c5224d6f2e71083005d996b5c9d28f9
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-07 02:44:17 -0400

    343. Asserting Reserved Tickets

--- src/routes/__test__/new.test.ts
+++ src/routes/__test__/new.test.ts
@@ -37,6 +37,28 @@ it("returns an error if the ticket does not exist", async () => {
     .expect(404);
 });
 
-it("returns an error if the ticket is already reserved", async () => {});
+it("returns an error if the ticket is already reserved", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: "lkjijhiknoih",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+
+  order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
 
 it("reserves a ticket", async () => {});
