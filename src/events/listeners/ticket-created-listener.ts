930ff53bda0da27a8afa986ddbaccdb5f329e232
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-08 04:13:46 -0400

    360. A Few More Reminders

--- src/events/listeners/ticket-created-listener.ts
+++ src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,13 @@
 import { Message } from "node-nats-streaming";
 import { Subjects, Listener, TicketCreatedEvent } from "@oldledger/common";
 import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = "orders-service";
+  queueGroupName = queueGroupName;
 
+  // msg contains the ack method that we use to acknowledge an
+  // event after it has been successfully processed
   onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
 }
