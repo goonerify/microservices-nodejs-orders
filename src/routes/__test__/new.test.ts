3010bd53704670b939663fc333485f5672eecca1
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-08 04:01:55 -0400

    356. Testing Event Publishing

--- src/routes/__test__/new.test.ts
+++ src/routes/__test__/new.test.ts
@@ -5,13 +5,7 @@ import { app } from "../../app";
 import mongoose from "mongoose";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
-
-const createOrder = () => {
-  return request(app).post("/api/orders").set("Cookie", global.signin()).send({
-    title: "asldkf",
-    price: 20,
-  });
-};
+import { natsWrapper } from "../../nats-wrapper";
 
 it("cannot be accessed if the user is not signed in", async () => {
   request(app).post("/api/orders").send({}).expect(401);
@@ -83,4 +77,19 @@ it("reserves a ticket", async () => {
   expect(orders).toHaveLength(1);
 });
 
-it.todo("emits an order created event");
+it("emits an order created event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
