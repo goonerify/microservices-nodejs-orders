f6e4f99586552b5dfacdcda0c811ff47c9053e0b
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-10 20:59:11 -0400

    416. Emitting the Order Cancelled Event

--- src/events/listeners/expiration-complete-listener.ts
+++ src/events/listeners/expiration-complete-listener.ts
@@ -8,6 +8,7 @@ import {
 import { Ticket } from "../../models/ticket";
 import { queueGroupName } from "./queue-group-name";
 import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   readonly subject = Subjects.ExpirationComplete;
@@ -21,7 +22,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     // We use this ticket created listener to implement data replication
     // in our local database for Tickets, so we don't have to reach across
     // to the main ticket service for information about tickets
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate("ticket");
 
     if (!order) {
       throw new Error("Order not found");
@@ -29,7 +30,23 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
 
     order.set({
       status: OrderStatus.Cancelled,
-      ticket: null,
+      // ticket: null,
+
+      // We don't have to dereference the ticket since our isReserved function
+      // that is used to determine is an order can be placed on a ticket includes
+      // the status criteria that an order must not be cancelled for it to be reserved.
+      // Therefore, we can leave in the ticket reference so that users can still see the
+      // ticket that the cancelled order was associated with
+    });
+
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
     });
 
     msg.ack();
