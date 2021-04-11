2a3a532f1dc12a0cca76d50e4fca381607f51244
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-10 21:13:02 -0400

    417. Testing the Expiration Complete Listener

---
+++ src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,35 @@
+import { ExpirationCompleteEvent } from "@oldledger/common";
+import mongoose from "mongoose";
+import { Order, OrderStatus } from "../../../models/order";
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper";
+import { ExpirationCompleteListener } from "../expiration-complete-listener";
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: "uihsuhs",
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+};
+
+it("", () => {});
